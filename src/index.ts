e95be225de9c0f79b5988dfb83162a603cae0261
Michael Solati <mkslt04@gmail.com>
2020-06-21 15:18:26 -0700

    feat: add functions for `onSnapshot` and `get` for geoqueries

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-export * from './types';
-export {
-  encodeDocumentAdd,
-  encodeDocumentSet,
-  encodeDocumentUpdate,
-} from './utils';
+export {geoQueryGet} from './api/query-get';
+export {geoQueryOnSnapshot} from './api/query-on-snapshot';
+export {GeoQuerySnapshot} from './api/snapshot';
+export * from './definitions';
+export * from './utils';
