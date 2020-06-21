3af42212000c996fb29eff80fe2c7d3215c631fa
Michael Solati <mkslt04@gmail.com>
2020-06-21 16:20:35 -0700

    refactor: export specific functions from `/utils`

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,10 @@ export {geoQueryGet} from './api/query-get';
 export {geoQueryOnSnapshot} from './api/query-on-snapshot';
 export {GeoQuerySnapshot} from './api/snapshot';
 export * from './definitions';
-export * from './utils';
+export {
+  encodeDocumentAdd,
+  encodeDocumentSet,
+  encodeDocumentUpdate,
+  validateLimit,
+  validateQueryCriteria,
+} from './utils';
