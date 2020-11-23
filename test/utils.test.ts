be5e026b2b6388ef4a9536e8518de00f42f03cb1
Michael Solati <mkslt04@gmail.com>
2020-11-22 17:09:01 -0800

    test(firebase): update firebase import in tests

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,5 +1,5 @@
 import * as chai from 'chai';
-import * as firebase from 'firebase/app';
+import firebase from 'firebase/app';
 import {hash as geohash} from 'geokit';
 import 'firebase/firestore';
 
